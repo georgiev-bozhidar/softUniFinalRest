11813e4680f452b14baba6a55e24268e7a3acfcd
Exdia <exd1a+github@proton.me>
2024-07-24 18:01:06 +0300

    Initial commit

---
+++ src/main/java/org/georgievbozhidar/softunifinal2rest/SoftUniFinal2RestApplication.java
@@ -0,0 +1,13 @@
+package org.georgievbozhidar.softunifinal2rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftUniFinal2RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftUniFinal2RestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/georgievbozhidar/softunifinal2rest/SoftUniFinal2RestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.georgievbozhidar.softunifinal2rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftUniFinal2RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
